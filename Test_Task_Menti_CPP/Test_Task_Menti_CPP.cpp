6792d4363a0afaf6679574cfa0f7ff44bc0cd2a5
RQUST <rqust@yandex.com>
2023-09-02 18:05:37 +0200

    Добавьте файлы проекта.

---
+++ Test_Task_Menti_CPP/Test_Task_Menti_CPP.cpp
@@ -0,0 +1,12 @@
+﻿// Test_Task_Menti_CPP.cpp: определяет точку входа для приложения.
+//
+
+#include "Test_Task_Menti_CPP.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
